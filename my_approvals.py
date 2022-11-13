a487782e08d7bb60b71afd2f7d265f2c0129b640
asafing1 <asaf.chen1@gmail.com>
2022-11-13 18:34:03 +0200

    POC - Managed to check all transactions of an address and spectated the amount Still need to find the name of the token Doesn't work well when there are too many transactions on the account (over 10,000)

--- my_approvals.py
+++ my_approvals.py
@@ -1,1 +1,13 @@
+# 1.
+# Approval                       > An event that is logged when an approve function was successful.
+# approve(spender, amount)       > Authorize transfers up to the given amount from the caller's tokens by the spender
+# transfer(to, amount)           > Moves tokens from the caller's account to an address
+# transferFrom(from, to, amount) > Moves tokens from one address to another address
+#
+# transferFrom moves tokens between 2 accounts, whereas transfer moves tokens from the sender to the recipient
+# transferFrom will only work if the amount to transfer from the transfering account has been approved
+
+
+# 2.
 import web3
+
--- test.py
+++ test.py
@@ -1,10 +1,47 @@
 from web3 import Web3
-from web3.auto import w3
+import web3._utils.filters as filters
+import asyncio
+import requests
+import json
+
+class EthConnecionError(Exception):
+    pass
+
+
+class InvalidAddressError(Exception):
+    pass
+
+
+async def get_block_chunk_logs(w3, address, chunk: int):
+    print(w3.eth.get_logs({"fromBlock": 0, "toBlock": 9999999, "address": address}))
 
 
 def test_1():
-    print(w3.isConnected())
-    print(w3.eth.get_block('last'))
+    alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/EzCaMUftsyJk3sDarB8-hbnirCK4G1TE"
+    address = "0x005e20fcf757b55d6e27dea9ba4f90c0b03ef852"
+    http_req = "https://api.etherscan.io/api?" \
+        "module=account&" \
+        "action=txlist&" \
+        f"address={address}&" \
+        "sort=asc&" \
+        "apikey=FREVNA4UCC6VZ9VA8CQ99FQREKDVRZK9BN"
+
+    w3 = Web3(Web3.HTTPProvider(alchemy_url))
+    if not w3.isConnected():
+        raise EthConnecionError(f"Connection to {alchemy_url} failed - try again later.")
+
+    resp = requests.get(http_req)
+    approval_transactions = []
+    for val in json.loads(resp.text)["result"]:
+        if val.get('functionName') and val['functionName'][:7] == 'approve':
+            approval_transactions.append(val['hash'])
+
+    for approval in approval_transactions:
+        receipt = w3.eth.get_transaction_receipt(approval)
+        for log in receipt.logs:
+            val = '∞' if '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff' else int(log.data, 16)
+            name = ''
+            print(f"approval on {name} on amount of {val}")
 
 
 test_1()
